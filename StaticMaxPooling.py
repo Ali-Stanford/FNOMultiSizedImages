4a6bc16b7bb9f90e68002adf713fd5e68a6ef3f0
Ali-Stanford <58951525+Ali-Stanford@users.noreply.github.com>
2024-04-11 14:11:18 -0700

    Update StaticMaxPooling.py

--- StaticMaxPooling.py
+++ StaticMaxPooling.py
@@ -13,10 +13,6 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 plt.rcParams['font.family'] = 'serif'
 plt.rcParams['font.serif'] = ['Times New Roman'] + plt.rcParams['font.serif']
-#from mpl_toolkits.mplot3d import Axes3D
-#import matplotlib.tri as tri
-#from mpl_toolkits import mplot3d
-
 #################
 
 import torch
