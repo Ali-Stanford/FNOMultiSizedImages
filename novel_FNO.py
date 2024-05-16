6716711107981fd108a7c95e6ef74cdbefaaefdb
Ali-Stanford <58951525+Ali-Stanford@users.noreply.github.com>
2024-05-15 22:22:12 -0700

    Update novel_FNO.py

--- novel_FNO.py
+++ novel_FNO.py
@@ -6,11 +6,15 @@
 #Citations:
 #If you use the code, please cite the following journal papers:
 
-#@article{kashefi2024novel,
-#title={A novel Fourier neural operator framework for classification of multi-sized images: Application to 3D digital porous media},
-#author={Kashefi, Ali and Mukerji, Tapan},
-#journal={arXiv preprint arXiv:2402.11568},
-#year={2024}}
+#@article{kashefi2024novelFNO,
+#  author = {Kashefi, Ali and Mukerji, Tapan},
+#  title = "{A novel Fourier neural operator framework for classification of multi-sized images: Application to three dimensional digital porous media}",
+#  journal = {Physics of Fluids},
+#  volume = {36},
+#  number = {5},
+#  year = {2024},
+#  doi = {10.1063/5.0203977},
+#  url = {https://doi.org/10.1063/5.0203977}}
 
 import os
 import random
